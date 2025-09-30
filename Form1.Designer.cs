97a4d4d86f5eeff36068e8addaa21a489398d6e6
Msalmi <mmsalmi66@gmail.com>
2025-09-30 23:29:06 +0100

    add picture box click handling

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,24 @@
             this.label3 = new System.Windows.Forms.Label();
             this.lbWinner = new System.Windows.Forms.Label();
             this.btRestart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -54,7 +72,7 @@
             this.label2.ForeColor = System.Drawing.Color.GreenYellow;
             this.label2.Location = new System.Drawing.Point(26, 138);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 50);
+            this.label2.Size = new System.Drawing.Size(123, 50);
             this.label2.TabIndex = 1;
             this.label2.Text = "Turn ";
             // 
@@ -65,7 +83,7 @@
             this.lbTurn.ForeColor = System.Drawing.Color.Honeydew;
             this.lbTurn.Location = new System.Drawing.Point(26, 228);
             this.lbTurn.Name = "lbTurn";
-            this.lbTurn.Size = new System.Drawing.Size(160, 50);
+            this.lbTurn.Size = new System.Drawing.Size(163, 50);
             this.lbTurn.TabIndex = 2;
             this.lbTurn.Text = "Player1";
             // 
@@ -76,7 +94,7 @@
             this.label3.ForeColor = System.Drawing.Color.GreenYellow;
             this.label3.Location = new System.Drawing.Point(26, 318);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 50);
+            this.label3.Size = new System.Drawing.Size(162, 50);
             this.label3.TabIndex = 3;
             this.label3.Text = "Winner";
             // 
@@ -87,7 +105,7 @@
             this.lbWinner.ForeColor = System.Drawing.Color.Honeydew;
             this.lbWinner.Location = new System.Drawing.Point(26, 408);
             this.lbWinner.Name = "lbWinner";
-            this.lbWinner.Size = new System.Drawing.Size(232, 50);
+            this.lbWinner.Size = new System.Drawing.Size(238, 50);
             this.lbWinner.TabIndex = 4;
             this.lbWinner.Text = "In Progress";
             // 
@@ -102,12 +120,121 @@
             this.btRestart.Text = "Restart";
             this.btRestart.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox1.Location = new System.Drawing.Point(286, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox2.Location = new System.Drawing.Point(446, 138);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox3.Location = new System.Drawing.Point(606, 138);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox4.Location = new System.Drawing.Point(286, 284);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseClick);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox5.Location = new System.Drawing.Point(286, 430);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseClick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox6.Location = new System.Drawing.Point(446, 284);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox6_MouseClick);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox7.Location = new System.Drawing.Point(606, 284);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox7_MouseClick);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox8.Location = new System.Drawing.Point(446, 430);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox8_MouseClick);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::tic_tac_toe_game.Properties.Resources.question_mark_96;
+            this.pictureBox9.Location = new System.Drawing.Point(606, 430);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 14;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox9_MouseClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1095, 663);
+            this.ClientSize = new System.Drawing.Size(1080, 645);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btRestart);
             this.Controls.Add(this.lbWinner);
             this.Controls.Add(this.label3);
@@ -117,6 +244,15 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +266,15 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lbWinner;
         private System.Windows.Forms.Button btRestart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,23 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using tic_tac_toe_game.Properties;
 
 namespace tic_tac_toe_game
 {
     public partial class Form1 : Form
     {
+         enum Turn { Player1,Player2 };
+         enum Result { Player1, Player2,Draw };
+
+        private Turn playerTurn=Turn.Player1;
+        private Result result;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void changeTicTacToe(PictureBox box)
+        {
+            switch (playerTurn)
+            {
+                case Turn.Player1:
+                    box.Image = Resources.X;
+                    playerTurn = Turn.Player2;
+                    lbTurn.Text=playerTurn.ToString();
+                    break;
+                case Turn.Player2:
+                    box.Image = Resources.O;
+                    playerTurn = Turn.Player1;
+                    lbTurn.Text = playerTurn.ToString();
+                    break;
+                default:
+                    MessageBox.Show("Game Over", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+            }
+        }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Color White = Color.White;
             Pen pen = new Pen(White);
             pen.StartCap = System.Drawing.Drawing2D.LineCap.NoAnchor;
             pen.Width = 5;
-
+            
 
             //painting X
 
@@ -35,9 +60,12 @@ namespace tic_tac_toe_game
 
             //
             //draw the table of tic tac toe 
-            e.Graphics.DrawLine(pen, 320, 105, 320, 410);
-            e.Graphics.DrawLine(pen,205,220,660,220);
-            e.Graphics.DrawLine(pen, 425, 105, 425, 410);
+            e.Graphics.DrawLine(pen, 320, 105, 320, 450);
+            e.Graphics.DrawLine(pen,205,220,550,220);
+            e.Graphics.DrawLine(pen, 425, 105, 425, 450);
+            e.Graphics.DrawLine(pen, 205, 335, 550, 335); 
+            
+
             
             
 
@@ -45,6 +73,50 @@ namespace tic_tac_toe_game
           
         }
 
-     
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox1);
+        }
+
+        private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe( pictureBox2);
+
+        }
+
+        private void pictureBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox3);
+        }
+
+        private void pictureBox4_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox4);
+        }
+
+        private void pictureBox6_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox6);
+        }
+
+        private void pictureBox7_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox7);
+        }
+
+        private void pictureBox5_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox5);
+        }
+
+        private void pictureBox8_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox8);
+        }
+
+        private void pictureBox9_MouseClick(object sender, MouseEventArgs e)
+        {
+            changeTicTacToe(pictureBox9);
+        }
     }
 }
