419fd0083ed7ad887497de2f2b3fc3a517d7417c
Msalmi <mmsalmi66@gmail.com>
2025-09-30 20:49:50 +0100

    draw the layout of the game

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbTurn = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbWinner = new System.Windows.Forms.Label();
+            this.btRestart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 24.1791F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(407, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic Tac Toe ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 19.8806F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label2.Location = new System.Drawing.Point(26, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Turn ";
+            // 
+            // lbTurn
+            // 
+            this.lbTurn.AutoSize = true;
+            this.lbTurn.Font = new System.Drawing.Font("Microsoft YaHei", 19.8806F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTurn.ForeColor = System.Drawing.Color.Honeydew;
+            this.lbTurn.Location = new System.Drawing.Point(26, 228);
+            this.lbTurn.Name = "lbTurn";
+            this.lbTurn.Size = new System.Drawing.Size(160, 50);
+            this.lbTurn.TabIndex = 2;
+            this.lbTurn.Text = "Player1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 19.8806F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label3.Location = new System.Drawing.Point(26, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 50);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Winner";
+            // 
+            // lbWinner
+            // 
+            this.lbWinner.AutoSize = true;
+            this.lbWinner.Font = new System.Drawing.Font("Microsoft YaHei", 19.8806F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWinner.ForeColor = System.Drawing.Color.Honeydew;
+            this.lbWinner.Location = new System.Drawing.Point(26, 408);
+            this.lbWinner.Name = "lbWinner";
+            this.lbWinner.Size = new System.Drawing.Size(232, 50);
+            this.lbWinner.TabIndex = 4;
+            this.lbWinner.Text = "In Progress";
+            // 
+            // btRestart
+            // 
+            this.btRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.97015F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRestart.Location = new System.Drawing.Point(26, 498);
+            this.btRestart.Name = "btRestart";
+            this.btRestart.Size = new System.Drawing.Size(183, 65);
+            this.btRestart.TabIndex = 5;
+            this.btRestart.Text = "Restart";
+            this.btRestart.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 513);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1095, 663);
+            this.Controls.Add(this.btRestart);
+            this.Controls.Add(this.lbWinner);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbTurn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbTurn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbWinner;
+        private System.Windows.Forms.Button btRestart;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,16 +19,32 @@ namespace tic_tac_toe_game
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Color Blue = Color.Blue; 
-            Pen pen=new Pen(Blue);
+            Color White = Color.White;
+            Pen pen = new Pen(White);
             pen.StartCap = System.Drawing.Drawing2D.LineCap.NoAnchor;
-            pen.Width = 4;
+            pen.Width = 5;
+
+
+            //painting X
+
+            e.Graphics.DrawLine(pen, 20,30, 70, 80);
+            e.Graphics.DrawLine(pen, 70, 30, 20, 80);
+
+            // painting O
+            e.Graphics.DrawEllipse(pen,720, 30, 56, 56);
+
+            //
+            //draw the table of tic tac toe 
+            e.Graphics.DrawLine(pen, 320, 105, 320, 410);
+            e.Graphics.DrawLine(pen,205,220,660,220);
+            e.Graphics.DrawLine(pen, 425, 105, 425, 410);
             
-            Point start = new Point(100, 100);
-            Point end=new Point(100, 400);
-            e.Graphics.DrawEllipse(pen, 10, 100,100,100);
-            e.Graphics.DrawLine(pen, 119, 80, 119, 200);
-            e.Graphics.DrawRectangle(pen, 200, 10, 300, 40);
+            
+
+            
+          
         }
+
+     
     }
 }
