099c004ed6f3f600f42b391e07f9126ae132a055
Msalmi <mmsalmi66@gmail.com>
2025-10-01 14:35:30 +0100

    add restart Game

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,12 +14,12 @@ namespace tic_tac_toe_game
 {
     public partial class Form1 : Form
     {
-         enum Turn { Player1,Player2 };
-         enum Result { Player1, Player2,Draw };
+        enum Turn { Empty,Player1, Player2};
+        enum Result { Player1, Player2,Draw };
 
         private Turn playerTurn=Turn.Player1;
         private Result result;
-        private Turn[,] Board = { { Turn.Player1, Turn.Player2,Turn.Player2 }, { Turn.Player1, Turn.Player2 ,Turn.Player1}, { Turn.Player1, Turn.Player2,Turn.Player1 } };
+        private Turn[,] Board = new Turn[3, 3];
         public Form1()
         {
             InitializeComponent();
@@ -31,18 +32,31 @@ namespace tic_tac_toe_game
             {
                 case Turn.Player1:
                     box.Image = Resources.X;
-                    playerTurn = Turn.Player2;
-                    lbTurn.Text=playerTurn.ToString();
+                    
+                   
                     break;
                 case Turn.Player2:
                     box.Image = Resources.O;
-                    playerTurn = Turn.Player1;
-                    lbTurn.Text = playerTurn.ToString();
+                   
+                  
                     break;
+                 
                 default:
                     MessageBox.Show("Game Over", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     break;
             }
+        }
+        private void changePlayerTurn()
+        {
+            switch (playerTurn) { 
+                case Turn.Player1:
+                    playerTurn=Turn.Player2;
+                    break;
+                case Turn.Player2:
+                    playerTurn=Turn.Player1;
+                    break;
+            }
+            lbTurn.Text = playerTurn.ToString();
         }
         private void setCordinateInBoard(short x, short y,Turn player)
         {
@@ -73,75 +87,102 @@ namespace tic_tac_toe_game
             e.Graphics.DrawLine(pen, 205, 335, 550, 335); 
         }
 
-        private bool checkWinner()
+        private Turn checkWinner()
         {   
             //check rowz
             for(int row = 0; row < 2; row++)
             {
-                if (Board[row,0]==playerTurn & Board[row,1]==playerTurn & Board[row, 2] == playerTurn)
-                {
-                    return true;
-                }
+               
+                    if (Board[row, 0] == playerTurn & Board[row, 1] == playerTurn & Board[row, 2] == playerTurn)
+                    {
+                        
+                        return playerTurn;
+                    }
+               
 
             }
             //check columns 
             for(int column = 0; column < 2; column++)
             {
-                if(Board[0,column]==playerTurn & Board[1,column]==playerTurn & Board[2, column] == playerTurn)
-                {
-                    return true;
-                }
+                
+                     if(Board[0,column]==playerTurn & Board[1,column]==playerTurn & Board[2, column] == playerTurn)
+                     {
+                         return playerTurn;
+                     }
+
+                 
             }
             //checks diagnols
             if(Board[0,0]==playerTurn & Board[1,1]==playerTurn & Board[2,2]==playerTurn)
-                { return true; }
+                { return playerTurn; }
             if (Board[0,2]==playerTurn & Board[1,1]==playerTurn & Board[2, 0] == playerTurn)
             {
-                return true;
+                return playerTurn;
             }
-            return false;
+            return Turn.Empty;
         }
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            
             changeTicTacToe(pictureBox1);
             setCordinateInBoard(0, 0, playerTurn);
-            
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
+            {
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            changePlayerTurn();
         }
 
         private void pictureBox2_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe( pictureBox2);
             setCordinateInBoard(0, 1, playerTurn);
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
+            {
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            changePlayerTurn();
         }
 
         private void pictureBox3_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe(pictureBox3);
             setCordinateInBoard(0, 2, playerTurn);
-            if (checkWinner())
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game");
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn();
         }
 
         private void pictureBox4_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe(pictureBox4);
-            setCordinateInBoard(1,0,playerTurn);
-            if (checkWinner())
+
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game"," ",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn() ;
+
         }
         private void pictureBox5_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe(pictureBox5);
             setCordinateInBoard(1, 1, playerTurn);
-            if (checkWinner())
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -150,10 +191,14 @@ namespace tic_tac_toe_game
         {
             changeTicTacToe(pictureBox6);
             setCordinateInBoard(1,2,playerTurn);
-            if (checkWinner())
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn();
+
 
         }
 
@@ -161,30 +206,39 @@ namespace tic_tac_toe_game
         {
             changeTicTacToe(pictureBox7);
             setCordinateInBoard(2,0,playerTurn);
-            if (checkWinner())
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn();
         }
 
         private void pictureBox8_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe(pictureBox8);
             setCordinateInBoard(2,1,playerTurn);
-            if (checkWinner())
+            Turn Winner=checkWinner();
+            if (Winner!= Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn();
         }
 
         private void pictureBox9_MouseClick(object sender, MouseEventArgs e)
         {
             changeTicTacToe(pictureBox9);
             setCordinateInBoard(2,2,playerTurn);
-            if (checkWinner())
+            Turn Winner = checkWinner();
+            if (Winner != Turn.Empty)
             {
-                MessageBox.Show($"{playerTurn} wins the game", " ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                MessageBox.Show($"{Winner} wins the game", "Game Over ", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            changePlayerTurn();
         }
     }
 }
